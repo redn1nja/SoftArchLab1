b3c6feefe422e4f6c0122be276c5dad3a03c1e18
redn1njaa <ostap.seryvko@ucu.edu.ua>
2024-01-31 18:52:18 +0200

    added facade's post request

--- microservices/src/main.rs
+++ microservices/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+
+#[post("/", format="plain", data = "<msg>")]
+fn post(msg: &str) -> String {
+    let s = std::fmt::format(format_args!("Hello, {}!", msg));
+    s
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![post])
+
 }
