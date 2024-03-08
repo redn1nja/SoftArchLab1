99aa411c4fb58659b4da97ad377e0f5f06cdca71
redn1njaa <ostap.seryvko@ucu.edu.ua>
2024-03-08 14:41:07 +0200

    added more verbosity

--- logging/src/main.cpp
+++ logging/src/main.cpp
@@ -14,24 +14,27 @@ int main(int argc, char* argv[])
     auto client = hazelcast::new_client(std::move(config)).get();
     auto map = client.get_map("map").get();
 
-    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)([&map](){
+    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)([&map, &address](){
         auto val = map->entry_set<std::string, std::string>().get();
         std::string result;
+        CROW_LOG_INFO << "GET request received by logging service connected to hazelcast node " << address << "\n";
         result = std::accumulate(val.begin(), val.end(), ""s, [](const std::string &result, auto &entry){
             return (result + entry.second + "\n");
         });
         return crow::response(200, result);
     });
 
-    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::POST)([&map](const crow::request &req){
+    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::POST)([&map, &address](const crow::request &req){
         auto x = crow::json::load(req.body);
         if (!x && !x.has("uuid") && !x.has("msg"))
             return crow::response(400);
+        CROW_LOG_INFO << "POST request received by logging service connected to hazelcast node " << address << " writing values" \
+                                                                        << x["uuid"].s() << " " << x["msg"].s() << "\n";
         map->put<std::string, std::string>(
                 x["uuid"].s().operator std::string(),
                 x["msg"].s().operator std::string())
                 .get();
         return crow::response(200);
     });
-    app.bindaddr("0.0.0.0").port(port).multithreaded().run();
+    app.bindaddr("0.0.0.0").port(port).loglevel(crow::LogLevel::INFO).multithreaded().run();
 }
